5d9712a769f30092b86a06722034e2dbedd8ff64
Kishore Kumar K-Manager-IT-Chennai-Chola <kishorek@cholamandalam.com>
2018-03-19 16:18:40 +0530

    Flask Chatterbot Added

--- script.py
+++ script.py
@@ -1,40 +1,14 @@
 from flask import Flask, render_template
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app =Flask(__name__)
 
-@app.route('/plot/')
-def plot():
-    from pandas_datareader import data
-    import datetime
-    from bokeh.plotting import figure, output_file, show
-    from bokeh.embed import components
-    from bokeh.resources import CDN
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
-    start=datetime.datetime(2016,10,1)
-    end=datetime.datetime(2017,3,10)
+english_bot.set_trainer(ChatterBotCorpusTrainer)
+english_bot.train("chatterbot.corpus.english")
 
-    df=data.DataReader(name="GOOG",data_source="google",start=start,end=end)
-
-    df_bull=df[df.Close>=df.Open]
-    df_bear=df[df.Open>df.Close]
-
-    p=figure(x_axis_type='datetime', height=300, width=1000,title='Candlestick Chart',sizing_mode='scale_width')
-    #p.grid.grid_line_alpha=0.5
-
-    hours_12=12*60*60*1000
-
-    p.segment(df.index, df.High, df.index, df.Low, color='black')
-    p.rect(x=df_bull.index,y=((df_bull.Open+df_bull.Close)/2),
-           width=hours_12,height=abs(df_bull.Close-df_bull.Open),color='#CCFFFF',line_color='black')
-
-    p.rect(x=df_bear.index,y=((df_bear.Open+df_bear.Close)/2),
-           width=hours_12,height=abs(df_bear.Open-df_bear.Close),color='#FF3333',line_color='black')
-
-    script1,div1=components(p)
-    cdn_js=CDN.js_files[0]
-    cdn_css=CDN.css_files[0]
-    return render_template("plot.html",
-    script1=script1, div1=div1,cdn_css=cdn_css,cdn_js=cdn_js)
 
 @app.route('/')
 def home():
@@ -47,6 +21,15 @@ def about():
 @app.route('/policy/')
 def policy():
     return render_template("policy.html")
+
+@app.route("/index/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
 
 if __name__ == "__main__":
     app.run(debug=True)
